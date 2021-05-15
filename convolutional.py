f9b269747b3c80a7868319d940c6fc1afd123e5e
YoungRaeKimm <33543992+YoungRaeKimm@users.noreply.github.com>
2021-05-16 00:17:01 +0900

    convolution layer 구현

---
+++ convolutional.py
@@ -0,0 +1,88 @@
+import numpy as np
+import copy
+
+
+class Conv():
+
+    def __init__(self, num_stride, padding, num_filter, filter_size, input_shape):
+        self.stride = stride
+        self.padding = padding  # 'same' or 'valid'
+        self.padding_shape = self.get_pad_shape()
+        self.num_filter = num_filter
+        self.filter_size = filter_size  # [a,b,c]
+
+        self.weights = np.reshape(
+            np.random.normal(0, 0.05, filter_size[0] * filter_size[1] * filter_size[2] * num_filter),
+            (filter_size[0], filter_size[1], filter_size[2], num_filter))
+        # height, width, channel, num_filter
+        self.bias = np.reshape(np.random.normal(0, 0.05, num_filter), (num_filter))
+        self.dweihts = np.zeros_like(self.weights)
+        self.dbias = np.zeros_like(self.bias)
+
+        self.input_shape = input_shape  # batch_size, height, width, channel
+        self.output_shape = self.get_output_shape(input_shape)  # batch_size, height, width, filter
+
+        self.inputt = np.zeros_like(input_shape)
+        # self.input_col = None
+
+    def get_output_shape(self, input_shape):  # batch_size, height, width, channel
+        if self.padding == 'same':
+            return [input_shape[0], input_shape[1], input_shape[2], num_filter]
+            # batch number, height number, width number, filter number
+        elif self.padding == 'valid':
+            return [input_shape[0], (input_shape[1] - weights.shape[0]) // self.stride + 1,
+                    (input_shape[2] - weights.shape[1]) // self.stride + 1, self.num_filter]
+
+    def get_pad_shape(self):
+        if self.padding == 'same':
+            return ((self.weights.shape[0] - 1) // 2, (self.weights.shape[1] - 1) // 2)
+        elif self.padding == 'valid':
+            return (0, 0)
+
+    def forward(self, inputt):
+        self.inputt = copy.deepcopy(inputt)
+
+        w = np.transpose(self.weights, (3, 2, 0, 1))  # num_filter, channel, height, width
+        input_padded = np.pad(inputt, pad_width=(
+        (0, 0), (self.padding_shape[0], padding_shape[0]), (padding_shape[1], padding_shape[1]), (0, 0)))
+
+        for i in range(self.output_shape[1]):
+            for j in range(self.output_shape[2]):
+                height_start = i * self.stride
+                height_end = height_start + self.weights.shape[0]
+                width_start = j * self.stride
+                width_end = width_start + self.weights.shape[1]
+
+                output[:, i, j, :] = np.sum(
+                    input_padded[:, h_start:h_end, w_start:w_end, :, np.newaxis] *
+                    self.weights[np.newaxis, :, :, :],
+                    axis=(1, 2, 3)
+                )
+
+        return output + self.bias
+
+    def backward(self, loss):  # batch, height, width, num_filter
+
+        input_padded = np.pad(inputt, pad_width=(
+        (0, 0), (self.padding_shape[0], padding_shape[0]), (padding_shape[1], padding_shape[1]), (0, 0)))
+        output = np.zeros_like(input_padded)
+
+        for i in range(loss.shape[1]):
+            for j in range(loss.shape[2]):
+                h_start = i * self._stride
+                h_end = h_start + h_f
+                w_start = j * self._stride
+                w_end = w_start + w_f
+                output[:, h_start:h_end, w_start:w_end, :] += np.sum(
+                    self.weights[np.newaxis, :, :, :, :] *
+                    loss[:, i:i + 1, j:j + 1, np.newaxis, :],
+                    axis=4
+                )
+                self.dweights += np.sum(
+                    input_padded[:, h_start:h_end, w_start:w_end, :, np.newaxis] *
+                    loss[:, i:i + 1, j:j + 1, np.newaxis, :],
+                    axis=0
+                )
+
+        self.dweights /= n
+        return output[:, pad[0]:pad[0] + self.inputt.shape[1], pad[1]:pad[1] + self.inputt.shape[2], :]

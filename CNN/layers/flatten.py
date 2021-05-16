4ee2424d54c24903390bd516b52711edc78ed7fb
jungchunkim <64156407+jungchunkim@users.noreply.github.com>
2021-05-17 03:17:33 +0900

    flatten_layer

---
+++ CNN/layers/flatten.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class Flatten:
+
+    def __init__(self):
+        # 입력받을 데이터의 shape를 받을 변수
+        self.prev_shape = ()
+
+    def forward_pass(self, prev_arr):
+        # Conv, pooling 이 끝난 데이터(n차원)을 입력받아서 1차원으로 변환한다.
+        self.prev_shape = prev_arr.shape
+        
+        return np.ravel(prev_arr).reshape(prev_arr.shape[0], -1)
+
+    def backward_pass(self, after_arr):
+        # 변환하기 전의 shape 로 1차원 array를 reshape 한다.
+        return after_arr.reshape(self.prev_shape)

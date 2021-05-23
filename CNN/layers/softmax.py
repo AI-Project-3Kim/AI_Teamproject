eeab407194db68a17879744fcd45c877988649bf
jungchunkim <64156407+jungchunkim@users.noreply.github.com>
2021-05-24 03:56:36 +0900

    Update softmax.py

--- CNN/layers/softmax.py
+++ CNN/layers/softmax.py
@@ -4,7 +4,9 @@ class Softmax():
     
     def __init__(self):
         self.softmax_output= None
-
+        self.dweight = None
+        self.dbias = None
+        
     def forward(self, prev_arr):
         # prev_arr을 softmax 함수를 처리한다.
         # 매우 큰 수가 되면 계산에 문제가 될 수 있기 때문에 최댓값을 빼준다. (결국 같은 값)
@@ -21,3 +23,9 @@ class Softmax():
         # softmax 값 - target 값 한 결과가 softmax loss의 역전파 
         # (Sequential.py에 시행)
         return after_arr
+    def get_gradient(self):
+        if self.dweight is None or self.dbias is None:
+            return None
+        g_w = self.dweight
+        g_b = self.dbias
+        return g_w,g_b
